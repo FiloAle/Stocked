61a006ac4ba08d41d58cf280da0566c2a2b4f574
filippoaleee <filippoalessandrini.fa@gmail.com>
2021-01-26 21:14:04 +0100

    New icon

--- app/src/main/java/com/stocked/MainActivity.kt
+++ app/src/main/java/com/stocked/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                R.id.nav_scanner, R.id.nav_inventory, R.id.nav_status), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/com/stocked/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/stocked/ui/gallery/GalleryFragment.kt
@@ -21,7 +21,7 @@ class GalleryFragment : Fragment() {
     ): View? {
         galleryViewModel =
                 ViewModelProvider(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val root = inflater.inflate(R.layout.fragment_inv, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
