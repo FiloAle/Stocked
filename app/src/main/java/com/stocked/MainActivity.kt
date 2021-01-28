1d5549508f73bdb8434eb6afdf6c068e7e56be7b
filoale <filippoalessandrini.fa@gmail.com>
2021-01-28 21:01:04 +0100

    Edited FAB Message.

--- app/src/main/java/com/stocked/MainActivity.kt
+++ app/src/main/java/com/stocked/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Edit your inventory", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
