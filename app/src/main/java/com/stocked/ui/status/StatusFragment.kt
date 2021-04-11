8107d37507f78722b458a995d90e0efbc5480114
filoale <filippoalessandrini.fa@gmail.com>
2021-04-11 20:27:46 +0200

    Updated UI and optimized instructions.

--- app/src/main/java/com/stocked/ui/status/StatusFragment.kt
+++ app/src/main/java/com/stocked/ui/status/StatusFragment.kt
@@ -1,10 +1,12 @@
 package com.stocked.ui.status
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.stocked.LoginActivity
@@ -23,9 +25,11 @@ class StatusFragment : Fragment() {
 
     private lateinit var btnCheck: Button
     private lateinit var loadingDialog: LoadingDialog
-    private lateinit var ip : String
-    private var port by Delegates.notNull<Int>()
+    private lateinit var txtStatus: TextView
+    private val ip : String = LoginActivity.ip
+    private val port = LoginActivity.port
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -35,6 +39,8 @@ class StatusFragment : Fragment() {
 
         loadingDialog = activity?.let { LoadingDialog(it) }!!
         btnCheck = root.findViewById(R.id.btnCheck)
+        txtStatus = root.findViewById(R.id.txtStatus)
+        txtStatus.text = getString(R.string.connected) + " " + getString(R.string.to) + " " + ip
         btnCheck.setOnClickListener {
             GlobalScope.launch(Dispatchers.Default)
             {
@@ -51,9 +57,9 @@ class StatusFragment : Fragment() {
                     )
 
                     if (MainActivity.socket.isConnected) {
+                        txtStatus.text = getString(R.string.connected) + " " + getString(R.string.to) + " " + ip
                         GlobalScope.launch(Dispatchers.Main)
                         {
-                            Toast.makeText(activity, getString(R.string.connected), Toast.LENGTH_SHORT).show()
                             loadingDialog.dismissDialog()
                         }
                     }
@@ -67,9 +73,6 @@ class StatusFragment : Fragment() {
                         ).show()
                     }
                     try {
-                        ip = LoginActivity.ip
-                        port = LoginActivity.port
-
                         try {
                             if (ip != "" && port != 0) {
                                 val serverIP = InetAddress.getByName(ip)
@@ -79,12 +82,12 @@ class StatusFragment : Fragment() {
                                 if (MainActivity.socket.isConnected) {
                                     GlobalScope.launch(Dispatchers.Main)
                                     {
-                                        Toast.makeText(activity, getString(R.string.connected), Toast.LENGTH_SHORT).show()
+                                        Toast.makeText(activity, getString(R.string.connected) + " " + getString(R.string.to) + " " + ip, Toast.LENGTH_SHORT).show()
+                                        loadingDialog.dismissDialog()
                                     }
                                 }
                             }
                         } catch (ex: Exception) {
-                            port = 0
                             GlobalScope.launch(Dispatchers.Main)
                             {
                                 Toast.makeText(
