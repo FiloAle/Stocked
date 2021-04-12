71b52600f5d87581a75c4cd47c033f7a1c0f8950
FiloAle <filippoalessandrini.fa@gmail.com>
2021-04-12 18:20:00 +0200

    Updated translations.

--- app/src/main/java/com/stocked/ui/add/AddFragment.kt
+++ app/src/main/java/com/stocked/ui/add/AddFragment.kt
@@ -62,7 +62,7 @@ class AddFragment : Fragment() {
         val productAmount : String = addView.findViewById<EditText>(R.id.dttProductQuantity).text.toString()
 
         if (productCode == ""){
-            Toast.makeText(requireContext(), "codice prodotto mancante", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), getString(R.string.missing_prd_code), Toast.LENGTH_SHORT).show()
             return
         } else if (productName == "") {
             Toast.makeText(requireContext(), getString(R.string.missing_prd_name), Toast.LENGTH_SHORT).show()
